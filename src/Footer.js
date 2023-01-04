20a85fcea10ab03d992902179a029bac7fee35ee
Artem Goncharenko <goncharenko.art@gmail.com>
2023-01-04 14:05:39 -0500

    🎩👞 header and footer

---
+++ src/Footer.js
@@ -0,0 +1,29 @@
+
+export const Footer = () => {
+    return (
+        <footer>
+            <img src="" alt="footer" />
+            <h3>Doormat navigation</h3>
+            <ul>
+                <li><a href={'#'}>Home</a></li>
+                <li><a href={'#'}>About</a></li>
+                <li><a href={'#'}>Menu</a></li>
+                <li><a href={'#'}>Reservations</a></li>
+                <li><a href={'#'}>Order online</a></li>
+                <li><a href={'#'}>Login</a></li>
+            </ul>
+            <h3>Contact</h3>
+            <ul>
+                <li><a href={'#'}>Address</a></li>
+                <li><a href={'#'}>Phone number</a></li>
+                <li><a href={'#'}>Email</a></li>
+            </ul>
+            <h3>Social media links</h3>
+            <ul>
+                <li><a href={'#'}>Address</a></li>
+                <li><a href={'#'}>Phone number</a></li>
+                <li><a href={'#'}>Email</a></li>
+            </ul>
+        </footer>
+    );
+}
---
+++ src/Header.js
@@ -0,0 +1,8 @@
+
+export const Header = () => {
+    return (
+        <header>
+            <img src="" alt="logo" />
+        </header>
+    );
+}
